874e786522790d42b6044cd60e7cc4239adc04a3
Andrea Brandolini <andreabrandolini15@gmail.com>
2025-11-20 13:38:30 +0100

    created mvc for cluster
    created file ClusterModel
    created file ClusterService

---
+++ src/main/java/Model/ClusterModel.java
@@ -0,0 +1,48 @@
+package Model;
+
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.Waypoint;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * Rappresenta un cluster di fermate sulla mappa.
+ * Implementa Waypoint così può essere disegnato sulla mappa.
+ */
+public class ClusterModel implements Waypoint {
+
+    private final Set<StopWaypoint> stops = new HashSet<>();
+    private GeoPosition center;
+
+    public ClusterModel(Set<StopWaypoint> stops) {
+        if (stops != null && !stops.isEmpty()) {
+            this.stops.addAll(stops);
+            calculateCenter();
+        }
+    }
+
+    private void calculateCenter() {
+        double sumLat = 0;
+        double sumLon = 0;
+        for (StopWaypoint stop : stops) {
+            sumLat += stop.getPosition().getLatitude();
+            sumLon += stop.getPosition().getLongitude();
+        }
+        this.center = new GeoPosition(sumLat / stops.size(), sumLon / stops.size());
+    }
+
+    @Override
+    public GeoPosition getPosition() {
+        return center;
+    }
+
+    public Set<StopWaypoint> getStops() {
+        return Collections.unmodifiableSet(stops);
+    }
+
+    public int getSize() {
+        return stops.size();
+    }
+}
---
+++ src/main/java/Service/ClusterService.java
@@ -0,0 +1,42 @@
+package Service;
+
+import Model.ClusterModel;
+import Model.StopWaypoint;
+import org.jxmapviewer.JXMapViewer;
+
+import org.jxmapviewer.viewer.GeoPosition;
+
+import java.awt.geom.Point2D;
+import java.util.*;
+
+/**
+ * Esegue il clustering dei waypoint secondo la griglia.
+ */
+public class ClusterService {
+
+    /**
+     * Raggruppa i waypoint visibili in cluster usando griglia basata sui pixel
+     */
+    public static Set<ClusterModel> createClusters(List<StopWaypoint> stops, JXMapViewer map, int gridSizePx) {
+        Map<String, Set<StopWaypoint>> grid = new HashMap<>();
+
+        for (StopWaypoint stop : stops) {
+            GeoPosition pos = stop.getPosition();
+            Point2D pt = map.getTileFactory().geoToPixel(pos, map.getZoom());
+
+            int gx = (int) (pt.getX() / gridSizePx);
+            int gy = (int) (pt.getY() / gridSizePx);
+
+            String key = gx + "_" + gy;
+
+            grid.computeIfAbsent(key, k -> new HashSet<>()).add(stop);
+        }
+
+        Set<ClusterModel> clusters = new HashSet<>();
+        for (Set<StopWaypoint> cellStops : grid.values()) {
+            clusters.add(new ClusterModel(cellStops));
+        }
+
+        return clusters;
+    }
+}
